2b83e12e06ea734a23bdf0bd25f2a0565aee94d0
Kirill Tikhonov <slimline@inbox.ru>
2020-04-18 21:10:59 +0300

    autopep8

--- __main__.py
+++ __main__.py
@@ -1,57 +1,66 @@
 from telegram.ext import Updater, CommandHandler
 
-import rates, settings
+import rates
+import settings
+
 
 def get_request_kwargs(proxy):
-   return{
-      'proxy_url':proxy,
-      # Optional, if you need authentication:
-      'urllib3_proxy_kwargs': {
-          'assert_hostname': 'False',
-          'cert_reqs': 'CERT_NONE'
-          # 'username': 'user',
-          # 'password': 'password'
-      }
+    return{
+        'proxy_url': proxy,
+        # Optional, if you need authentication:
+        'urllib3_proxy_kwargs': {
+            'assert_hostname': 'False',
+            'cert_reqs': 'CERT_NONE'
+            # 'username': 'user',
+            # 'password': 'password'
+        }
     }
+
 
 def start(update, context):
     update.message.reply_text('Hi! \n /get \n /help')
 
+
 def get(update, context):
     data = rates.get_rates_cached()
-    output="List of currencies: \n"
+    output = "List of currencies: \n"
     for currency, rate in data.items():
-         if rate != None:
-           output+=("currency: %s rate: %s \n" %(currency,rate))
+        if rate != None:
+            output += ("currency: %s rate: %s \n" % (currency, rate))
     update.message.reply_text(output)
+
 
 def help(update, context):
     update.message.reply_text('Hi! \n /get \n /help')
+
 
 def register_handlers(dp):
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("get", get))
     dp.add_handler(CommandHandler("help", help))
 
-def routine(requestargs = {}):
-    updater = Updater(settings.TOKEN,request_kwargs=requestargs, use_context=True)
+
+def routine(requestargs={}):
+    updater = Updater(
+        settings.TOKEN, request_kwargs=requestargs, use_context=True)
     register_handlers(updater.dispatcher)
     # Start the Bot
     updater.start_polling()
     updater.idle()
 
 
-def main(currs = ["BTC","ETH"] ):
-    #if proxy list is present, use proxies
+def main(currs=["BTC", "ETH"]):
+    # if proxy list is present, use proxies
     if settings.PROXIES_LIST:
-         for proxy in settings.PROXIES_LIST:
+        for proxy in settings.PROXIES_LIST:
             try:
-               print(proxy)
-               routine(get_request_kwargs(proxy))
+                print(proxy)
+                routine(get_request_kwargs(proxy))
             except:
                 print("connection error")
     else:
         routine()
+
 
 if __name__ == "__main__":
     # execute only if run as a script
--- rates.py
+++ rates.py
@@ -1,35 +1,40 @@
-import http.client, cachetools.func, requests, json
+import http.client
+import cachetools.func
+import requests
+import json
 from concurrent.futures import ThreadPoolExecutor
 
 
 USD = "USDT"
 URL = "https://api.binance.com"
-API_TYPES = {"price" : "/api/v3/avgPrice","time" : "/api/v3/time"}
+API_TYPES = {"price": "/api/v3/avgPrice", "time": "/api/v3/time"}
 
 
 @cachetools.func.ttl_cache(maxsize=20, ttl=60)
-def get_rates_cached(currs = ["BTC","ETH"]):
+def get_rates_cached(currs=["BTC", "ETH"]):
     return get_rates_concurrent(currs)
 
-def get_rates_concurrent(currs = ["BTC","ETH"]):
+
+def get_rates_concurrent(currs=["BTC", "ETH"]):
     executor = ThreadPoolExecutor(max_workers=4)
     if isinstance(currs, str):
-       currency_pairs= currs + USD
+        currency_pairs = currs + USD
     else:
-      currency_pairs = [currency + USD for currency in currs ]
-    result =  zip(currency_pairs, executor.map(make_request, currency_pairs))
-    return dict(result) 
+        currency_pairs = [currency + USD for currency in currs]
+    result = zip(currency_pairs, executor.map(make_request, currency_pairs))
+    return dict(result)
 
-def get_rates(currs = ["BTC","ETH"]):
-    currency_pairs =   [currency + USD for currency in currs ]
-    results = [ {symbol:make_request(symbol)} for symbol in currency_pairs]
-    return results 
+
+def get_rates(currs=["BTC", "ETH"]):
+    currency_pairs = [currency + USD for currency in currs]
+    results = [{symbol: make_request(symbol)} for symbol in currency_pairs]
+    return results
+
 
 def make_request(symbol):
     try:
-      params = {"symbol":symbol }
-      r = requests.get(url = URL+API_TYPES["price"] , params=params) 
-      return  json.loads(r.content)["price"]
+        params = {"symbol": symbol}
+        r = requests.get(url=URL+API_TYPES["price"], params=params)
+        return json.loads(r.content)["price"]
     except:
-      return None
-  
+        return None
